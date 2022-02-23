9d6d9ad3d9ff0d5eef022aa172924e54a7fdd949
Michael Griehm <griehmmj@gmail.com>
2022-02-23 13:49:29 -0500

    Can't configure a value for "location": its value will be decided automatically based on the result of applying this configuration.

--- iac/event-hub.tf
+++ iac/event-hub.tf
@@ -38,7 +38,6 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_resource_group" "rg" {
   name     = local.rg_name
-  location = var.location
 }
 
 resource "azurerm_eventhub_namespace" "ehns" {
