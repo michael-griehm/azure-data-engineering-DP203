ec9539a55cd24879657a7b3111251545a8290e95
Michael Griehm <griehmmj@gmail.com>
2022-02-25 17:24:21 -0500

    Added shared access policy for the Event Hub.

--- iac/event-hub.tf
+++ iac/event-hub.tf
@@ -14,3 +14,12 @@ resource "azurerm_eventhub" "eh" {
   message_retention   = 1
 }
 
+resource "azurerm_eventhub_authorization_rule" "example" {
+  name                = "sas-function-stream"
+  namespace_name      = azurerm_eventhub_namespace.example.name
+  eventhub_name       = azurerm_eventhub.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  listen              = false
+  send                = true
+  manage              = false
+}
